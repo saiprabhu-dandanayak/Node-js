d07063976a6ae9c457dfca2b8bbc76715b30bc94
saiprabhu-dandanayak <saiprabhu.dandanayak@zemosolabs.com>
2025-01-06 20:39:50 +0530

    📝 feat : implemented static views

---
+++ static-views/src/index.ts
@@ -0,0 +1,9 @@
+import express , {Request , Response} from "express"
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(3000, () =>{
+    console.log("Server is running on port 3000")
+})
